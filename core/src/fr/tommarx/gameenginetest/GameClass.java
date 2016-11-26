441483a6719116e80120cde773f0d451d1d64832
Tom <tom.marx@hotmail.fr>
2016-11-26 20:08:42 +0100

    GameClass

---
+++ core/src/fr/tommarx/gameenginetest/GameClass.java
@@ -0,0 +1,16 @@
+package fr.tommarx.gameenginetest;
+
+import fr.tommarx.gameengine.Game.Game;
+
+public class GameClass extends Game {
+
+	public void create () {
+		init();
+		setScreen(new ScreenTest());
+	}
+
+	
+	public void dispose () {
+		stop();
+	}
+}
