ee1950ce922566883ed204da9c46a3a5148761dd
Tom <tom.marx@hotmail.fr>
2016-12-04 18:02:45 +0100

    Map prise en charge components (tous sauf lights)

--- core/src/fr/tommarx/gameengine/Util/MapReader.java
+++ core/src/fr/tommarx/gameengine/Util/MapReader.java
@@ -2,13 +2,17 @@ package fr.tommarx.gameengine.Util;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 
 import java.util.ArrayList;
 
 import fr.tommarx.gameengine.Components.BoxBody;
+import fr.tommarx.gameengine.Components.BoxRenderer;
+import fr.tommarx.gameengine.Components.CircleBody;
 import fr.tommarx.gameengine.Components.SpriteRenderer;
+import fr.tommarx.gameengine.Components.Text;
 import fr.tommarx.gameengine.Components.Transform;
 import fr.tommarx.gameengine.Game.EmptyGameObject;
 
@@ -58,7 +62,35 @@ public class MapReader {
                     if (bodyString.equals("Kinematic")) {
                         body = BodyDef.BodyType.KinematicBody;
                     }
-                    object.addComponent(new BoxBody(object, Float.parseFloat(getComponentPropertie(component, "width")), Float.parseFloat(getComponentPropertie(component, "height")), body));                }
+                    object.addComponent(new BoxBody(object, Float.parseFloat(getComponentPropertie(component, "width")), Float.parseFloat(getComponentPropertie(component, "height")), body));
+                }
+                if (type.equals("CircleBody")) {
+                    BodyDef.BodyType body = BodyDef.BodyType.StaticBody;
+                    String bodyString = getComponentPropertie(component, "bodyType");
+                    if (bodyString.equals("Static")) {
+                        body = BodyDef.BodyType.StaticBody;
+                    }
+                    if (bodyString.equals("Dynamic")) {
+                        body = BodyDef.BodyType.DynamicBody;
+                    }
+                    if (bodyString.equals("Kinematic")) {
+                        body = BodyDef.BodyType.KinematicBody;
+                    }
+                    object.addComponent(new CircleBody(object, Float.parseFloat(getComponentPropertie(component, "radius")), body));
+                }
+                if (type.equals("Text")) {
+                    object.addComponent(new Text(object, Gdx.files.internal(getComponentPropertie(component, "fontFile")), Integer.parseInt(getComponentPropertie(component, "size")), getComponentPropertie(component, "text"),
+                            new Color(Float.parseFloat(getComponentPropertie(component, "r")),
+                                    Float.parseFloat(getComponentPropertie(component, "g")),
+                                    Float.parseFloat(getComponentPropertie(component, "b")),
+                                    Float.parseFloat(getComponentPropertie(component, "a")))));
+                }
+                if (type.equals("BoxRenderer")) {
+                    object.addComponent(new BoxRenderer(object, Float.parseFloat(getComponentPropertie(component, "width")), Float.parseFloat(getComponentPropertie(component, "height")), new Color(Float.parseFloat(getComponentPropertie(component, "r")),
+                            Float.parseFloat(getComponentPropertie(component, "g")),
+                            Float.parseFloat(getComponentPropertie(component, "b")),
+                            Float.parseFloat(getComponentPropertie(component, "a")))));
+                }
             }
 
             go.add(object);
